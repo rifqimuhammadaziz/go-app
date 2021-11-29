3b72f98121d1815f049f4c13e4a8dde86c3db7f7
Rifqi Muhammad Aziz <rifqimuhammadaziz@gmail.com>
2021-11-30 05:47:45 +0700

    go application for testing dependency

---
+++ go.mod
@@ -0,0 +1,5 @@
+module github.com/rifqimuhammadaziz/go-app
+
+go 1.16
+
+require github.com/rifqimuhammadaziz/go-module/v3 v3.0.0 // indirect
---
+++ main.go
@@ -0,0 +1,11 @@
+package main
+
+import (
+	"fmt"
+
+	gosayhellomodule "github.com/rifqimuhammadaziz/go-module/v3"
+)
+
+func main() {
+	fmt.Println(gosayhellomodule.SayHello("Rifqi Muhammad"))
+}
